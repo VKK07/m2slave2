0e998424cf3f6ab25340e2d16d4752264eaca271
kiran kumar venkatesan <165485010+VKK07@users.noreply.github.com>
2024-06-11 09:47:47 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,5 @@
 sudo yum -y install git
 sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
-cd /home/ec2-user/workspace/m2slave-1
+cd /home/ec2-user/workspace/m2slave2
 sudo pip3 install -r requirements.txt
 python3 app.py

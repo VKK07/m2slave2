f6c8cdb9669bede4369aef4823fc527888e0a6bb
kiran kumar venkatesan <165485010+VKK07@users.noreply.github.com>
2024-06-11 10:16:22 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo yum -y install git
 sudo yum install python3-pip -y
-sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
-cd /home/ec2-user/workspace/m2slave2
-sudo pip3 install -r requirements.txt
+git clone https://github.com/sumanthgangireddyGS/fish.git
+cd fish
+pip3 install -r requirements.txt
 screen -m -d python3 app.py
